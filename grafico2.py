81be107e75827379d98bb0395ef3ff29c6e9b31e
EstebanC99 <estebancarignani99gmail.com>
2020-10-28 11:19:20 -0300

    Creacion del grafico

---
+++ grafico2.py
@@ -0,0 +1,19 @@
+import cv2
+import matplotlib.pyplot as plt
+
+imagen_original = cv2.imread(r'Australia.jpg')
+imagen_orginal = cv2.cvtColor(imagen_original, cv2.COLOR_BGR2RGB)
+
+imagen_gris = cv2.cvtColor(imagen_original, cv2.COLOR_RGB2GRAY)
+
+_, binaria = cv2.threshold(imagen_gris, 225, 255, cv2.THRESH_BINARY_INV)
+
+plt.imshow(binaria, cmap="gray")
+plt.show()
+
+contornos, _ = cv2.findContours(binaria, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+imagen_original = cv2.drawContours(imagen_original, contornos, -1, (0, 255, 0), 2)
+
+plt.imshow(imagen_original)
+plt.show()
