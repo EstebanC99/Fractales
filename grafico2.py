283ee66393ed041cf24af51d03e61e49e222768d
estebancarignani99@gmail.com <estebancarignani99@gmail.com>
2020-10-28 20:17:59 -0300

    Archivo comentado

--- grafico2.py
+++ grafico2.py
@@ -1,21 +1,24 @@
+#IMPORTAMOS LIBRERIAS NECESARIAS
 import cv2
 import matplotlib.pyplot as plt
 
+#CARGAMOS IMAGENES
 imagen_original = cv2.imread(r'Australia.jpg')
 imagen_blanca = cv2.imread(r'Fondo.jpg')
-imagen_orginal = cv2.cvtColor(imagen_original, cv2.COLOR_BGR2RGB)
 
+#CONVERTIMOS DE BGR A RGB Y A GRIS
+imagen_orginal = cv2.cvtColor(imagen_original, cv2.COLOR_BGR2RGB)
 imagen_gris = cv2.cvtColor(imagen_original, cv2.COLOR_RGB2GRAY)
 
+#CREACION DE IMAGEN BINARIA DE GRIS
 _, binaria = cv2.threshold(imagen_gris, 225, 255, cv2.THRESH_BINARY_INV)
 
-plt.imshow(binaria, cmap="gray")
-plt.show()
-
+#EXTRACCION DEL CONTORNO DE LA IMAGEN
 contornos, _ = cv2.findContours(binaria, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
-
+#DIBUJA EL CONTORNO SOBRE EL FONDO BLANCO
 imagen_blanca = cv2.drawContours(imagen_blanca, contornos, -1, (0, 0, 0), 2)
 
+#MUESTRA DE LA IMAGEN FINAL
 plt.imshow(imagen_blanca)
 plt.show()
